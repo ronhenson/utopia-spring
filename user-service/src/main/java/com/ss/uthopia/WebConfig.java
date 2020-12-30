6228c36a94eea13261f7713be9ef183fe2b75390
Derek Lance <derek.lance@smoothstack.com>
2020-12-30 15:14:21 -0800

    Fix tests, remove unused imports

--- user-service/src/main/java/com/ss/uthopia/WebConfig.java
+++ user-service/src/main/java/com/ss/uthopia/WebConfig.java
@@ -1,13 +1,9 @@
 package com.ss.uthopia;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration

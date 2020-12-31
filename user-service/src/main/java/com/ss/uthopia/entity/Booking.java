15dcf2c28552023f02baa2e0fbbccb0277eb9cb0
Derek Lance <derek.lance@smoothstack.com>
2020-12-30 16:22:46 -0800

    Update UserService for new database schema

--- user-service/src/main/java/com/ss/uthopia/entity/Booking.java
+++ user-service/src/main/java/com/ss/uthopia/entity/Booking.java
@@ -4,68 +4,62 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "booking")
-@Table(name = "booking")
+@Table(name = "tbl_booking")
 public class Booking {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    @Column(name = "booking_id")
-    private long bookingId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long bookingId;
 
-    @ManyToOne()
-    @JoinColumns({
-            @JoinColumn(name="booker_id", referencedColumnName="user_id")
-    })
-    private User user;
+	@ManyToOne()
+	@JoinColumns({ @JoinColumn(name = "bookerId", referencedColumnName = "userId") })
+	private User user;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "booking_travelers",
-            joinColumns = {@JoinColumn(name = "booking_id", referencedColumnName = "booking_id")},
-    inverseJoinColumns = {@JoinColumn(name = "traveler_id")})
-    private List<Traveler> travelers;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "tbl_bookings_has_travelers", joinColumns = {
+			@JoinColumn(name = "bookingId") }, inverseJoinColumns = { @JoinColumn(name = "travelerId") })
+	private List<Traveler> travelers;
 
-    @Column(name = "is_active")
-    private boolean isActive;
+	private boolean isActive;
 
-    @Column(name = "stripe_id")
-    private String stripeId;
+	private String stripeId;
 
-    public long getBookingId() {
-        return bookingId;
-    }
+	public long getBookingId() {
+		return bookingId;
+	}
 
-    public void setBookingId(long bookingId) {
-        this.bookingId = bookingId;
-    }
+	public void setBookingId(long bookingId) {
+		this.bookingId = bookingId;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User users) {
-        this.user = users;
-    }
+	public void setUser(User users) {
+		this.user = users;
+	}
 
-    public boolean isActive() {
-        return isActive;
-    }
+	public boolean isActive() {
+		return isActive;
+	}
 
-    public void setActive(boolean active) {
-        isActive = active;
-    }
+	public void setActive(boolean active) {
+		isActive = active;
+	}
 
-    public String getStripeId() {
-        return stripeId;
-    }
+	public String getStripeId() {
+		return stripeId;
+	}
 
-    public void setStripeId(String stripeId) {
-        this.stripeId = stripeId;
-    }
+	public void setStripeId(String stripeId) {
+		this.stripeId = stripeId;
+	}
 
-    public List<Traveler> getTravelers() {
-        return travelers;
-    }
+	public List<Traveler> getTravelers() {
+		return travelers;
+	}
 
-    public void setTravelers(List<Traveler> travelers) {
-        this.travelers = travelers;
-    }
+	public void setTravelers(List<Traveler> travelers) {
+		this.travelers = travelers;
+	}
 }

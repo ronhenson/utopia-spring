809fb55b3cc2f3488e76c82026e776163eb6fcc7
Arun Mavumkal <av8r88@gmail.com>
2021-01-17 17:41:54 -0500

    modified routes and entities in users service to conform to new db schema

--- user-service/src/main/java/com/smoothstack/utopia/entity/User.java
+++ user-service/src/main/java/com/smoothstack/utopia/entity/User.java
@@ -1,7 +1,16 @@
 package com.smoothstack.utopia.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity(name = "User")
 @Table(name = "tbl_users")
 public class User {
@@ -10,61 +19,13 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 
-	private String name;
-	
-	private String username;
-	
+	private String firstName;
+
+	private String lastName;
+
 	private String password;
 
 	private String email;
-	
-	private int role;
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public int getRole() {
-		return role;
-	}
-
-	public void setRole(int role) {
-		this.role = role;
-	}
+	private int userRole;
 }

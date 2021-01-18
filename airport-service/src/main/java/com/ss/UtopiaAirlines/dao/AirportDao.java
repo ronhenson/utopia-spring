94b0e5d91fa9644cb4ac220755835970873655fe
Derek Lance <derek.lance@smoothstack.com>
2021-01-17 16:37:17 -0800

    Modify GET /airport route to handle search queries

--- airport-service/src/main/java/com/ss/UtopiaAirlines/dao/AirportDao.java
+++ airport-service/src/main/java/com/ss/UtopiaAirlines/dao/AirportDao.java
@@ -6,6 +6,7 @@ package com.ss.UtopiaAirlines.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ss.UtopiaAirlines.enitity.Airport;
@@ -18,5 +19,15 @@ import com.ss.UtopiaAirlines.enitity.Airport;
 public interface AirportDao extends JpaRepository<Airport, String>{
 	
 	List<Airport> findByCityIgnoreCase(String city);
+
+	@Query(value = 
+		"select * from tbl_airport "
+			+ "where iataIdent like %?1% "
+			+ "or city like %?1% "
+			+ "or name like %?1% "
+			+ "limit 15",
+			nativeQuery = true
+	)
+	List<Airport> search(String query);
 
 }

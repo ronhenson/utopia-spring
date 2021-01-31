80a6a895879e58ea2678eee56709eb318186e4b2
Ron Henson <ron.henson@gmail.com>
2021-01-31 14:42:35 -0800

    Implemented Airport Service Unit test

--- airport-service/src/test/java/com/ss/UtopiaAirlines/service/AirportServiceTests.java
+++ airport-service/src/test/java/com/ss/UtopiaAirlines/service/AirportServiceTests.java
@@ -11,15 +11,10 @@ import java.util.Optional;
 import com.ss.UtopiaAirlines.dao.AirportDao;
 import com.ss.UtopiaAirlines.entity.Airport;
 import com.ss.UtopiaAirlines.exceptions.ResourceDoesNotExistsException;
-import com.ss.UtopiaAirlines.exceptions.ResourceExistsException;
-import com.ss.UtopiaAirlines.service.AirportService;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
-import com.ss.UtopiaAirlines.controller.AirportController;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,55 +39,63 @@ public class AirportServiceTests {
 
   private final Airport AIRPORT_NOT_FOUND = new Airport(NOT_FOUND, "Town, CA", "Town Airport");
 
-  private final Airport AIRPORT_ADD_TEST = new Airport(IATALIDENTDEL, "New Town", "New Town Airport");
-
   @BeforeEach
   void setAirportDaoOutput() {
     when(airportDao.findById(NOT_FOUND)).thenReturn(Optional.empty());
     when(airportDao.findById(IATALIDENT)).thenReturn(Optional.of(AIRPORT));
     when(airportDao.save(AIRPORT)).thenReturn(AIRPORT);
-    when(airportDao.findAll()).thenReturn(List.of(AIRPORT));
+    when(airportDao.existsById(AIRPORT.getIataIdent())).thenReturn(true);
+    when(airportDao.existsById(AIRPORT_NOT_FOUND.getIataIdent())).thenReturn(false);
     when(airportDao.search(QUERY)).thenReturn(List.of(AIRPORT));
+    when(airportDao.findAll()).thenReturn(List.of(AIRPORT));
   }
 
   @Test
-  @DisplayName("get airport by iatalIdent, not found, expect exception")
+  @DisplayName("get airport by iatalIdent, not found, expect False")
   void test1() {
-    assertThrows(ResourceDoesNotExistsException.class, () -> {
-     airportService.getAirportById(NOT_FOUND);
-     System.out.println("How did I get here ");
-    });
+    assertFalse(airportService.getAirportById(NOT_FOUND).isPresent());
   }
 
-  // @Test
-  // @DisplayName("get airport by iatalIdent, expect body")
-  // void test2() {
-  //   Optional<Airport> airport = airportService.getAirportById(IATALIDENT);
-  //   assertThat(AIRPORT, samePropertyValueAs(airport.get()));
-
-  // }
+  @Test
+  @DisplayName("get airport by iatalIdent, expect True")
+  void test2() {
+    Optional<Airport> airport = airportService.getAirportById(IATALIDENT);
+    assertTrue(airport.isPresent());
+  }
 
  @Test
- @DisplayName("add airport by Id, expect body")
+ @DisplayName("update airport, expect Exception")
  void test3() {
-   assertThrows(ResourceExistsException.class, () -> {
-     airportService.addAirport(AIRPORT_ADD_TEST);
+   assertThrows(ResourceDoesNotExistsException.class, () -> {
+     airportService.updateAirport(AIRPORT_NOT_FOUND);
    });
  }
 
  @Test
- @DisplayName("delete airport by Id, expect body")
- void test4() {
+ @DisplayName("update airport, expect True")
+ void test4() throws ResourceDoesNotExistsException {
+     assertThat(AIRPORT, samePropertyValuesAs(airportService.updateAirport(AIRPORT)));
+ }
+
+ @Test
+ @DisplayName("delete airport by Id, expect exception ")
+ void test5() {
    assertThrows(ResourceDoesNotExistsException.class, () -> {
      airportService.deleteAirport(IATALIDENTDEL);
    });
  }
 
  @Test
- @DisplayName("query 15 at a time, expect list")
- void test5() {
-   assertThrows(ResourceDoesNotExistsException.class, () -> {
-     airportService.deleteAirport(QUERY);
-   });
+ @DisplayName("query by airport id and/or name , expect list")
+ void test6() {
+     List<Airport> airportList = airportService.search(QUERY);
+     assertThat(airportList.get(0), samePropertyValuesAs(AIRPORT));
+ }
+
+ @Test
+ @DisplayName("Find all, expect list")
+ void test7() throws ResourceDoesNotExistsException {
+    List<Airport> airportList = airportService.getAllAirports();
+    assertThat(airportList.get(0), samePropertyValuesAs(AIRPORT));
  }
 }

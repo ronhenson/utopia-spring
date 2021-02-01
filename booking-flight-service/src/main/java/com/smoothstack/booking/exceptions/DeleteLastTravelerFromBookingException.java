1e88bf7d832deba3ec39697ad7bcdd00ea049c9a
dereklance <derek.lance@smoothstack.com>
2021-01-31 19:47:43 -0800

    Prevent last traveler in booking from being deleted

---
+++ booking-flight-service/src/main/java/com/smoothstack/booking/exceptions/DeleteLastTravelerFromBookingException.java
@@ -0,0 +1,10 @@
+package com.smoothstack.booking.exceptions;
+
+public class DeleteLastTravelerFromBookingException extends RuntimeException {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -2031477152295735013L;
+	
+}

0727033374606f02c018d7ec19d786380fd6e1ab
dereklance <derek.lance@smoothstack.com>
2021-02-05 14:33:14 -0800

    Integration tests for user service

---
+++ user-service/src/test/resources/data.sql
@@ -0,0 +1,8 @@
+insert into tbl_users
+(userId, password, email, firstName, lastName, enabled, locked, userRole)
+values
+
+(1, 'password1', 'user@1.com', 'user', 'one', 1, 0, 0),
+(2, 'password2', 'user@2.com', 'user', 'two', 1, 0, 0),
+(3, 'password3', 'user@3.com', 'username', 'three', 1, 0, 0),
+(4, 'password4', 'user@4.com', 'user', 'four', 1, 0, 0);
